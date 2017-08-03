eead3d9d15c9f6dbc32644a115e84f1914795f13
shadowwzw <miditab@yandex.ru>
2017-08-03 23:40:15 +0400

    fix

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,45 +2,16 @@ export const GET_IMAGES_START = 'GET_IMAGES_START';
 export const GET_IMAGES_FINISH = 'GET_IMAGES_FINISH';
 export const GET_IMAGES_ERROR = 'SELECT_REDDIT';
 
-// export const selectReddit = reddit => ({
-//   type: SELECT_REDDIT,
-//   reddit
-// })
-//
-// export const invalidateReddit = reddit => ({
-//   type: INVALIDATE_REDDIT,
-//   reddit
-// })
-//
-// export const requestPosts = reddit => ({
-//   type: REQUEST_POSTS,
-//   reddit
-// })
-//
-// export const receivePosts = (reddit, json) => ({
-//   type: RECEIVE_POSTS,
-//   reddit,
-//   posts: json.data.children.map(child => child.data),
-//   receivedAt: Date.now()
-// })
-//
-// const fetchPosts = reddit => dispatch => {
-//   dispatch(requestPosts(reddit))
-//   return fetch(`https://www.reddit.com/r/${reddit}.json`)
-//     .then(response => response.json())
-//     .then(json => dispatch(receivePosts(reddit, json)))
-// }
-//
-// const shouldFetchPosts = (state, reddit) => {
-//   const posts = state.postsByReddit[reddit]
-//   if (!posts) {
-//     return true
-//   }
-//   if (posts.isFetching) {
-//     return false
-//   }
-//   return posts.didInvalidate
-// }
+export const INC_RATE = 'INC_RATE';
+export const DEC_RATE = 'DEC_RATE';
+
+export const incRate = () => ({
+  type: INC_RATE
+});
+
+export const decRate = () => ({
+  type: DEC_RATE
+});
 
 export const fetchImages = () => async (dispatch, getState) => {
   dispatch({ type: GET_IMAGES_START });
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { fetchImages } from '../actions'
+import { fetchImages, incRate, decRate } from '../actions'
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
   static propTypes = {
     images: PropTypes.object.isRequired,
   };
@@ -14,12 +19,22 @@ class App extends Component {
     dispatch(fetchImages());
   }
 
+  handleClick(e) {
+    console.log(e.type);
+    switch(e.type){
+      case 'click': break;
+      case 'contextmenu': break;
+      default: break;
+    }
+  }
+
+
   render() {
     console.log('this.props in app = ', this.props);
     const { images: { listOfImages, loading } } = this.props;
     return (
       <div>
-        {listOfImages && listOfImages.map((image, index) => <div key={image.id} className="square bg" style={{backgroundImage: `url("${image.src}")`}}><div className="content">{image.rate}</div></div>)}
+        {listOfImages && listOfImages.map((image, index) => <div key={image.id} onClick={this.handleClick} onContextMenu={this.handleClick} className="square bg" style={{backgroundImage: `url("${image.src}")`}}><div className="content">{image.rate}</div></div>)}
       </div>
     )
   }
